9c874d0e157b98686321ff8a40e07e62c6136a1b
Andre Franco <andre.franco.dev@gmail.com>
2023-05-27 08:45:43 -0600

    se definio el modelo empleado

---
+++ ProyectoHDP/Models/Empleados.cs
@@ -0,0 +1,20 @@
+﻿namespace ProyectoHDP.Models
+{
+    public class Empleados
+    {
+        public int iD { get; set; }
+        public string nombre { get; set; } = string.Empty;
+        public string tipoContrato { get; set; } = string.Empty;
+        public string pais { get; set; } = string.Empty;
+        public string empresa { get; set; } = string.Empty;
+        public double salario { get; set; }
+        public DateOnly fechaContrato { get; set; }
+        public DateOnly fechaRenuncia{ get; set; }
+        public DateOnly fechaEmision{ get; set; }
+
+        public Empleados()
+        {
+            
+        }
+    }
+}
